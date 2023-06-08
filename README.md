38555128b8ebe362723e3286acc5182491faa0ec
vivek <imvsingh20@gmail.com>
2023-06-08 20:35:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nike Brand Page
+
+![Brand Page]url('./Nike Brand Page.png)
---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+
+
+const Hero = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-button">
+          <button>Show Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Availability On</p>
+          <div className="brand-icons">
+            <img src="/public/images/amazon.png" alt="amazon-logo" />
+            <img src="/public/images/flipkart.png" alt="flipakrt-logo" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/hero-image.png" alt="shoe-image" />
+      </div>
+    </main>
+  );
+}
+
+export default Hero
